2e36666ae1848b75bfdb0e8a4bdd036101dd1411
shishoufeng <shishoufeng@szy.cn>
2020-06-11 17:33:20 +0800

    dev:修改跨进程传递数据代码

--- app/src/main/java/com/app/simple/codes/activity/task/SoundActivity.kt
+++ app/src/main/java/com/app/simple/codes/activity/task/SoundActivity.kt
@@ -1,8 +1,12 @@
 package com.app.simple.codes.activity.task
 
 import android.os.Bundle
+import android.os.IBinder
 import androidx.appcompat.app.AppCompatActivity
 import com.app.simple.codes.R
+import com.app.simple.codes.utils.BitmapBinder
+import com.app.simple.codes.utils.P
+import kotlinx.android.synthetic.main.activity_task_sound.*
 
 /**
  * Created by shishoufeng on 2020-05-25.
@@ -17,6 +21,14 @@ class SoundActivity :AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_sound)
+
+        val bundle = intent.extras?.getBundle("data")
+        val binder = bundle?.getBinder("big_bitmap")
+
+        P.outI(" binder = $binder")
+
+        img_tran.setImageBitmap((binder as BitmapBinder).bitmap)
+
     }
 
 
